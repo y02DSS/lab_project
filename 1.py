040c35a4b22fb78b978dc746151b2b61e0e524fe
y02DSS <57633706+y02DSS@users.noreply.github.com>
2021-09-15 14:05:03 +0200

    lines

--- 1.py
+++ 1.py
@@ -1,9 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 class lab8():
 	def __init__(self):
 		super().__init__()
 		self.k()
 
 	def k(self):
-		print(12)
+		x = np.linspace(-5, 5, 100)
+		y = (x - 2)*(x + 2)
+		fig, ax = plt.subplots()
+		ax.hlines(0, -5, 5, linewidth = 5)
+		ax.hlines(10, -5, 10, linewidth = 5)
+		ax.hlines(-10, -10, 15, linewidth = 5)
+		ax.vlines(10, -5, 10, linewidth = 5)
+		ax.vlines(-15, -15, 15, linewidth = 5)
+		edge = 25
+		plt.axis('equal')
+		ax.set_xlim(-edge, edge)
+		ax.set_ylim(-edge, edge)
+
+		plt.show()
 
 lab8()
