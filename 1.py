2e73ec52ca0ce5153d1cc3916a6fb92f34496886
Данила Улаев <ulaev03@mail.ru>
2021-09-16 11:39:19 +0200

    realshit

--- 1.py
+++ 1.py
@@ -8,7 +8,10 @@ class lab8():
 		super().__init__()
 		self.x = []
 		self.y = []
+		self.edge = 25
 		self.k()
+		self.a = 0
+		self.b = 0
 
 	def k(self):
 		x3 = np.linspace(-5, 5, 300)
@@ -27,40 +30,42 @@ class lab8():
 			self.x.append(i)
 			self.y.append(j)
 		fig, ax = plt.subplots()
-		edge = 25
 		plt.axis('equal')
-		ax.set_xlim(-edge, edge)
-		ax.set_ylim(-edge, edge)
+		ax.set_xlim(-self.edge, self.edge)
+		ax.set_ylim(-self.edge, self.edge)
 		plt.plot(x3, y3)
 		plt.plot(x1, y1)
 		plt.plot(x2, y2)
-		return collusion(self.x, self.y)
 
-	def circle(r,a,b):
+		return self.collusion(self.x, self.y)
+
+	def circle(self,a,b):
 		n = int(input("Введите число точек "))
-		self.x = np.linspace(-2*r,2*r,n)
-		self.y = np.linspace(-2*r,2*r,n)
-		X,Y = np.meshgrid(x, y)
-		fxy= (X-a)**2 + (Y-b)**2
-		plt.contour(X, Y, fxy, levels=[0])
-		plt.show()
+		r = np.linspace(0, self.edge, 100)
+		self.xc = np.linspace(-2*r,2*r,n)
+		self.yc = np.linspace(-2*r,2*r,n)
+		return xc, yc
 
-	def collusion():
-		while x != edge and y != edge:
-			draw()
+	def collusion(self, x, y):
+		while self.circle(self.a, self.b)[0] != x and self.circle(self.a, self.b)[1] != y:
+			for i,j in zip(x,y):
+				if self.circle(self.a,self.b).x1 == i and self.circle(self.a,self.b).y1 == j:
+					a = i
+					b = j
 
-	def draw():
+	def draw(self):
+		self.anim_object, = plt.plot([], [], '-', lw = 2)
 		fig, ax = plt.subplots()
 		anim_object, = plt.plot([], [], '-', lw = 2)
 		xdata, ydata = [], []
-		ani = FuncAnimation(fig, update, frames = np.arange(0, 5, 0.1), interval = 10)
+		ani = FuncAnimation(fig, self.update, frames = np.arange(0, 5, 0.1), interval = 10)
 		plt.axis('equal')
 		ax.set_xlim(-5, 5)
 		ax.set_ylim(-5, 5)
 		ani.save('lab_7_3.gif')
 
-	def update(frames):
-		anim_object.set_data(circle(frames)[0], circle(frames)[1])
+	def update(self, frames):
+		self.anim_object.set_data(self.circle(frames)[0], circle(frames)[1])
 		return anim_object,
 
 lab8()
