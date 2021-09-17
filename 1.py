31d07046ed16feccfa6c6a1a91dfba6dfe766cfd
y02DSS <57633706+y02DSS@users.noreply.github.com>
2021-09-17 10:24:42 +0200

    clear

--- 1.py
+++ 1.py
@@ -12,53 +12,59 @@ class lab8():
 		self.k()
 
 	def k(self):
-		x3 = np.linspace(-5, 5, 300)
+		x3 = np.linspace(-5, 5, 50)
 		y3 = x3**2/4-10
-		y1 = np.linspace(-3, 10, 300)
+		y1 = np.linspace(-3, 10, 50)
 		x1 = y1 - 5
-		y2 = np.linspace(-3, 10, 300)
+		y2 = np.linspace(-3, 10, 50)
 		x2 = -y1 + 20
 		for i, j in zip(x1, y1):
-			self.x.append(i)
-			self.y.append(j)
+			self.x.append(round(i,1))
+			self.y.append(round(j,1))
 		for i, j in zip(x2, y2):
-			self.x.append(i)
-			self.y.append(j)
+			self.x.append(round(i,1))
+			self.y.append(round(j,1))
 		for i, j in zip(x3, y3):
-			self.x.append(i)
-			self.y.append(j)
+			self.x.append(round(i,1))
+			self.y.append(round(j,1))
 		plt.axis('equal')
 		ax.set_xlim(-self.edge, self.edge)
 		ax.set_ylim(-self.edge, self.edge)
 		plt.plot(x3, y3)
 		plt.plot(x1, y1)
 		plt.plot(x2, y2)
-
 		return self.krug(self.x, self.y)
 
 
 	def krug(self, x0, y0):
+		c = 0
+		exitFlag=False
 		anim_object, = plt.plot([], [], '-', lw=2)
-		xdata, ydata = [], []
 
-
-		def update(frame, x0, y0):
-			print(1)
-			theta = np.linspace(0, 2 * np.pi, 100)
-			for i, j in zip(x0, y0):
-				for h in range(len(theta)):
-					xdata = (frame*np.cos(theta))
-					ydata = (frame*np.sin(theta))
-					if xdata[h] == i or ydata[h] == j:
+		def update(frame, x0, y0, c, exitFlag):
+			theta = np.linspace(0, 2 * np.pi, 50)
+			xdata = (frame*np.cos(theta))
+			ydata = (frame*np.sin(theta))
+			if c == 0:
+				for i, j in zip(x0, y0):
+					for h in range(len(theta)):
+						#print(round(xdata[h],1), round(ydata[h],1), '   ',i,j)
+						if round(xdata[h],1) == i and round(ydata[h],1) == j:
+							c = 1
+							exitFlag=True
+							break
+					if exitFlag:
 						break
-			anim_object.set_data(xdata, ydata)
+			anim_object.set_data(xdata[:50], ydata[:50])
+
+			
 			return anim_object,
 
 		ani = FuncAnimation(fig,
 							update,
 							frames=np.arange(0, 6, 0.1),
 							interval=10,
-							fargs = (x0, y0,),
+							fargs = (x0, y0, c, exitFlag,),
 							blit=True)
 		plt.show()
 		ani.save('lab7_2_b.gif')
